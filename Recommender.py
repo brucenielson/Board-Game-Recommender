7ac55de8fdb0217d7f17f9b60ba6d5eb3324b343
brucenielson <brucenielson@users.noreply.github.com>
2019-10-06 11:24:36 -0600

    Starting real work on Recommender system

--- Recommender.py
+++ Recommender.py
@@ -1,24 +1,33 @@
 import mysql.connector
 
-# Connect to the database and query a game.
-mydb = mysql.connector.connect(
-    host="dallas113.arvixeshared.com",
-    user="nikkala_genie",
-    passwd="RepMjhaqfM9y58m",
-    database="nikkala_boardgamegenie",
-    auth_plugin='mysql_native_password'
-)
+DEBUG = True
 
-gameids = [152]
+class GameDB:
 
-for gameid in gameids:
+    # Connect to the database and query a game.
+    # Make class variable
+    mydb = mysql.connector.connect(
+        host="dallas113.arvixeshared.com",
+        user="nikkala_genie",
+        passwd="RepMjhaqfM9y58m",
+        database="nikkala_boardgamegenie",
+        auth_plugin='mysql_native_password'
+    )
 
-    mycursor = mydb.cursor()
 
-    sql = "SELECT game.name, rating.user, rating.rating FROM game, rating WHERE game.game = rating.game and game.game = " + str(gameid);
-    mycursor.execute(sql)
-    result = mycursor.fetchall()
-    print("The results for game number " + str(gameid) + "are: ")
-    for x in result:
-        print(x)
+    def get_game_ratings(self, gameids):
+        for gameid in gameids:
 
+            mycursor = GameDB.mydb.cursor()
+
+            sql = "SELECT game.name, rating.user, rating.rating FROM game, rating WHERE game.game = rating.game and game.game = " + str(gameid);
+            mycursor.execute(sql)
+            result = mycursor.fetchall()
+            return result
+
+
+def main():
+    gameids = [152]
+    db = GameDB()
+    result=db.get_game_ratings(gameids)
+    print(result)
